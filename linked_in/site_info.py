58a4797e41c967f1d783d4acf7a826f093a38678
Jose <jleonardola@gmail.com>
2022-10-25 12:07:09 +0200

    Scrape by country codes as well

--- linked_in/site_info.py
+++ linked_in/site_info.py
@@ -5,16 +5,25 @@ LOGIN = 'https://www.linkedin.com/login'
 JOBS = "https://www.linkedin.com/jobs/search/?geoId=91000000&keywords=django&start=0"
 HOME = 'https://www.linkedin.com'
 WEBSITE_ALIAS = "linked_in"
-SEARCH_LINKS = (
-    'https://www.linkedin.com/jobs/search/?currentJobId=3328383281&f_E=2&geoId=91000000&keywords=django',
-    'https://www.linkedin.com/jobs/search/?currentJobId=3328383281&f_E=2&geoId=91000000&keywords=python',
-    'https://www.linkedin.com/jobs/search/?currentJobId=3328383281&f_E=2&geoId=91000000&keywords=flask',
-    'https://www.linkedin.com/jobs/search/?currentJobId=3328383281&f_E=2&geoId=91000000&keywords=api',
-    'https://www.linkedin.com/jobs/search/?currentJobId=3328383281&geoId=91000000&keywords=django',
-    'https://www.linkedin.com/jobs/search/?currentJobId=3328383281&geoId=91000000&keywords=python',
-    'https://www.linkedin.com/jobs/search/?currentJobId=3328383281&geoId=91000000&keywords=flask',
-    'https://www.linkedin.com/jobs/search/?currentJobId=3328383281&geoId=91000000&keywords=api',
-    "https://www.linkedin.com/jobs/search/?currentJobId=3313314409&distance=25&f_E=2&f_TPR=r604800&geoId=91000000&keywords=django&sortBy=R",
-    'https://www.linkedin.com/jobs/search/?currentJobId=3324837256&distance=25&f_E=4&f_TPR=r604800&geoId=91000000&keywords=django&sortBy=R',
 
+geo_ids = {
+    'netherlands': '102890719',
+    'sweden': '105117694',
+    'norway': '103819153',
+    'denmark': '104514075',
+    'spain': '105646813',
+
+}
+search_terms = "django,python,software,developer,automation,testing,game,api,flask".split(',')
+bases = (
+    'https://www.linkedin.com/jobs/search/?currentJobId=3328383281&f_E=2&geoId=__GEOID__&keywords=',
+    'https://www.linkedin.com/jobs/search/?currentJobId=3328383281&geoId=__GEOID__&keywords='
 )
+SEARCH_LINKS = list()
+for search_term in search_terms:
+    for base in bases:
+        for country, code in geo_ids.items():
+            search_link = f"{base}{search_term}".replace("__GEOID__", code)
+            SEARCH_LINKS.append(search_link)
+
+print("-")
