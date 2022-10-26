f105343537249fcfc22ea1d001dbd0f78bb52c92
Jose <jleonardola@gmail.com>
2022-10-26 13:03:31 +0200

    Always make a JobPostingApplicantChange entry

--- promote_me_not/display_jobs/models.py
+++ promote_me_not/display_jobs/models.py
@@ -8,7 +8,7 @@ from langdetect import detect
 class JobPosting(SoftDeleteObject, models.Model):
     job_id = models.IntegerField(unique=True, default=0)
     retrieval_date = models.DateTimeField(default=timezone.now)
-    first_seen = models.DateTimeField(default=timezone.now)
+    first_seen = models.DateTimeField(default=None)
     posted_date = models.DateTimeField(default=timezone.now)
     applicants = models.IntegerField(null=True)
     title = models.CharField(max_length=255, null=True)
@@ -31,17 +31,16 @@ class JobPosting(SoftDeleteObject, models.Model):
         return self.language_detected
 
     def update(self, other):
-        if other.applicants != self.applicants:
-            print(f"{self} has received {other.applicants - self.applicants} new applicants, saving.")
-            applicants_change = JobPostingApplicantChange(
-                offer_related=self,
-                previous_applicants=self.applicants,
-                previous_date=self.retrieval_date,
-                current_applicants=other.applicants,
-                current_date=other.retrieval_date,
+        print(f"{self} has received {other.applicants - self.applicants} new applicants, saving.")
+        applicants_change = JobPostingApplicantChange(
+            offer_related=self,
+            previous_applicants=self.applicants,
+            previous_date=self.retrieval_date,
+            current_applicants=other.applicants,
+            current_date=other.retrieval_date,
 
-            )
-            applicants_change.save()
+        )
+        applicants_change.save()
         self.retrieval_date = timezone.now()
 
         self.applicants = other.applicants
@@ -49,7 +48,7 @@ class JobPosting(SoftDeleteObject, models.Model):
 
     def save(self, *args, **kwargs):
         if not self.first_seen:
-            print(f"Found new entry: {self}")
+            print(f"New entry: {self}")
             self.first_seen = timezone.now()
             self.retrieval_date = timezone.now()
 
