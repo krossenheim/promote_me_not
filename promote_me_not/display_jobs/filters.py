c5f78b2900ef0d4b68cacdec5e91d97c5f0cb67d
Jose <jleonardola@gmail.com>
2022-10-26 11:34:42 +0200

    Change filters

--- promote_me_not/display_jobs/filters.py
+++ promote_me_not/display_jobs/filters.py
@@ -1,5 +1,6 @@
 import django_filters
 from .models import JobPosting
+from django.forms.widgets import TextInput
 
 
 class JobPostingFilter(django_filters.FilterSet):
@@ -11,15 +12,26 @@ class JobPostingFilter(django_filters.FilterSet):
                                                       lookup_expr='contains')
     description_contains4 = django_filters.CharFilter(field_name='description',
                                                       lookup_expr='contains')
-    description_contains_not = django_filters.CharFilter(field_name='description',
-                                                         lookup_expr='contains')
-    description_contains_not1 = django_filters.CharFilter(field_name='description',
-                                                          lookup_expr='contains')
-    description_contains_not2 = django_filters.CharFilter(field_name='description',
-                                                          lookup_expr='contains')
-    description_contains_not4 = django_filters.CharFilter(field_name='description',
-                                                          lookup_expr='contains')
+    description_contains_not = django_filters.CharFilter(
+        method='description_does_not_contain',
+        widget=TextInput(attrs={'placeholder': 'Does not contain'})
+    )
+    description_contains_not1 = django_filters.CharFilter(
+        method='description_does_not_contain',
+        widget=TextInput(attrs={'placeholder': 'Does not contain'})
+    )
+    description_contains_not2 = django_filters.CharFilter(
+        method='description_does_not_contain',
+        widget=TextInput(attrs={'placeholder': 'Does not contain'})
+    )
+    description_contains_not3 = django_filters.CharFilter(
+        method='description_does_not_contain',
+        widget=TextInput(attrs={'placeholder': 'Does not contain'})
+    )
+
+    def description_does_not_contain(self, qs, name, value):
+        return qs.exclude(description__contains=value)
 
     class Meta:
         model = JobPosting
-        exclude = ('pk',)
+        fields = tuple()
