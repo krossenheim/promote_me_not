a91f32874f21d249cff32ba1ba607c94ccefd421
jleonardola@gmail.com <jleonardola@gmail.com>
2022-11-02 18:46:29 +0100

    More later

--- linked_in/site_info.py
+++ linked_in/site_info.py
@@ -5,7 +5,7 @@ LOGIN = 'https://www.linkedin.com/login'
 HOME = 'https://www.linkedin.com'
 WEBSITE_ALIAS = "linked_in"
 JOB_TABS_CONTAINER_CLASSNAME = 'scaffold-layout__detail'
-MINIMUM_TIME_PER_PAGE_SECONDS = 0.8
+MINIMUM_TIME_PER_PAGE_SECONDS = 1
 
 geo_ids = {
     'finland' : '100456013',
