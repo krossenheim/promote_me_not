ec51f5cd50bd70cb315e4b5ecc42d53f022f618f
Jose <jleonardola@gmail.com>
2022-11-08 13:48:35 +0100

    Made some dockerfiles

--- the_scrapering/common/common.py
+++ the_scrapering/common/common.py
@@ -1,24 +1,14 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
-import os
-import platform
-job_saving_folder_path = '../jobs'
+from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# import platform
 
 
-def get_browser(option_arguments=("--headless","--no-sandbox",)) -> webdriver.Chrome:
-    executable_path = ChromeDriverManager().install() if platform.system() == 'Windows' else ChromeDriverManager(version='105.0.5195.19').install()
+def get_browser(option_arguments=("--headless", "--no-sandbox",)) -> webdriver.Chrome:
+    # manager = ChromeDriverManager() if platform.system() == 'Windows' else ChromeDriverManager(version='105.0.5195.19')
     chrome_options = Options()
     for argument in option_arguments:
         chrome_options.add_argument(argument)
-    br = webdriver.Chrome(executable_path=executable_path, options=chrome_options)
+    br = webdriver.Chrome(executable_path='the_scrapering/common/chromedriver', options=chrome_options)
     return br
-
-
-def create_destination_folders(website_alias):
-    website_jobs_save_path = f'{job_saving_folder_path}/{website_alias}'
-    if not os.path.isdir(f'{job_saving_folder_path}'):
-        os.mkdir(job_saving_folder_path)
-        os.mkdir(website_jobs_save_path)
-    elif not os.path.isdir(website_jobs_save_path):
-        os.mkdir(website_jobs_save_path)
